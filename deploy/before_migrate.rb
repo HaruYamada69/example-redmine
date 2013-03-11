55ef627c44d48c4de5963c2b3db19a87e18ce1af
Yusuke Ando <yandod@gmail.com>
2013-03-11 13:45:34 +0900

    add bundle install for first deploy

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,6 +4,7 @@ if !File.exist?("/usr/bin/rake") then
 end
 
 if !File.exist?("#{shared_path}/config/secret_token.rb") then
+  run "bundle install"
   run "#{rakecmd} generate_secret_token"
   run "mv #{release_path}/config/initializers/secret_token.rb #{shared_path}/config/secret_token.rb"
 end
